1cbcb76e953b62abdc34618f8bc370c9201cfe37
Soumya Gautam <101983954+soumyagautam@users.noreply.github.com>
2024-01-13 21:11:27 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 import google.generativeai as palm
 from flask import Flask, render_template, request
+from transformers import BartTokenizer, BartForConditionalGeneration
+from googlesearch import search
 
 API_KEY = "YOUR API KEY HERE"
 
@@ -7,12 +9,15 @@ palm.configure(api_key=API_KEY)
 models = [str(model.name) for model in palm.list_models()]
 model_id = models[1]
 
+tokenizer = BartTokenizer.from_pretrained("facebook/bart-large-cnn")
+model = BartForConditionalGeneration.from_pretrained("facebook/bart-large-cnn")
+
 app = Flask(__name__)
 
 
 @app.route("/")
 def hello_world():
-    return render_template("index.html")
+    return render_template("index.html", google="")
 
 
 @app.route("/submit", methods=["POST"])
@@ -31,7 +36,15 @@ def submit():
 
     result = completion.result
 
-    return render_template("index.html", output=result)
+    google_results = []
+    for j in search(prompt):
+        google_results.append(j)
+
+    input_ids = tokenizer.encode(result, return_tensors="pt", max_length=1024, truncation=True)
+    summary_ids = model.generate(input_ids, max_length=500, length_penalty=2.0, num_beams=4, early_stopping=True)
+    summary = tokenizer.decode(summary_ids[0], skip_special_tokens=True)
+
+    return render_template("index.html", output=result, ques=prompt, summary=summary, google=google_results)
 
 
 if __name__ == "__main__":
